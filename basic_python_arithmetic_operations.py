1c4eccf732f503801e965d24acaab4b74e8c50e2
fatehullah778866 <fatehmsd2522004@gmail.com>
2024-07-21 20:51:09 -0700

    My first commit

---
+++ basic_python_arithmetic_operations.py
@@ -0,0 +1,10 @@
+first_No=4
+Second_No=7
+Addition=first_No+Second_No
+print("Addition is:",Addition)
+Subtraction=first_No-Second_No
+print("Subtraction is:",Subtraction)
+Multiplication=first_No*Second_No
+print("Multiplication is:",Multiplication)
+Division=first_No/Second_No
+print("Division is:",Division)
